175b4dd0640bda3b2da3564b01e2b0d7431a0d58
Bethany Burrows <jedcburrows@gmail.com>
2025-01-13 11:56:45 +0000

    Added placeholder name & location information

--- src/Components/InfoTab/InfoTab.tsx
+++ src/Components/InfoTab/InfoTab.tsx
@@ -9,6 +9,15 @@ const InfoTab = () => {
         src={pikaplaceholder}
         alt="insert pokemon name here"
       />
+      <p>Pokemon name here</p>
+      <p className="infotab__listheader">
+        This pokemon can be found in the following locations:{" "}
+      </p>
+      <ul className="infotab__list">
+        <li className="infotab__location">Location 1</li>
+        <li className="infotab__location">Location 2</li>
+        <li className="infotab__location">Location 3</li>
+      </ul>
     </div>
   );
 };
