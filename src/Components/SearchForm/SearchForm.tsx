70ec81a05e18eeb5d93dfee4489c93ebaeb9d002
Bethany Burrows <jedcburrows@gmail.com>
2025-01-13 10:26:15 +0000

    Added initial layout styling to search form

--- src/Components/SearchForm/SearchForm.tsx
+++ src/Components/SearchForm/SearchForm.tsx
@@ -4,14 +4,18 @@ const SearchForm = () => {
   return (
     <div className="searchform">
       This is where the search form will go.
-      <input type="text" placeholder="Pokemon name here ..." />
-      <select>
+      <input
+        className="searchform__input"
+        type="text"
+        placeholder="Pokemon name here ..."
+      />
+      <select className="searchform__dropdown">
         <option value="pokemon1">pokemon1</option>
         <option value="pokemon2">pokemon2</option>
         <option value="pokemon3">pokemon3</option>
       </select>
       <p>This will appear when one of the above options is used</p>
-      <select>
+      <select className="searchform__dropdown">
         <option value="game1">game1</option>
         <option value="game2">game2</option>
         <option value="game3">game3</option>
